08b0e2407e342de1fd209e9be2fd72d2545e2ebc
Anthony Carminati <anthony@carminati.io>
2015-11-27 13:46:15 -0600

    flask site template in place

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# flask_site_template
+# Flask Site Template
 This is a template for starting a new flask project. Included is all of the basic backend structure. Not included is static files or templates.
--- app/site/routes.py
+++ app/site/routes.py
@@ -2,11 +2,9 @@ from flask import render_template
 from . import site
 import locale
 
-
 locale.setlocale(locale.LC_ALL, 'en_US')
 
 
-@site.route('/trader_performance')
-def trader_performance():
-
+@site.route('/')
+def index():
     return render_template('site/index.html')
